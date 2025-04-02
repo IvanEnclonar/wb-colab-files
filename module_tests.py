2714870823873972de81c4d150c26c99f64e6a55
ivanenclonar <paulivanenclonar@gmail.com>
2025-04-02 14:46:49 +0800

    Add additional test cases and checkpoints to module_tests.py

--- module_tests.py
+++ module_tests.py
@@ -63,3 +63,45 @@ def test12(logits_without_sae_recon, logits_no_saes):
     print("All tests passed!")
 
     submit.test_submit(12, mid=mid, eid=eid)
+
+def test13(show_top_logits, gpt2, gpt2_sae):
+    tests.test_show_top_logits(show_top_logits, gpt2, gpt2_sae)
+    print("All tests passed!")
+    submit.test_submit(13, mid=mid, eid=eid)
+
+def test14(prompts):
+    assert prompts["system"].startswith("We're studying neurons in a neural network.")
+    assert "<< new>>" in prompts["user"]
+    assert prompts["assistant"] == "this neuron fires on"
+
+    print("All tests passed!")
+    submit.test_submit(14, mid=mid, eid=eid)
+
+#Checkpoints are not required for this module
+def checkpoint1():
+    """Completed Attention SAEs subsubsection"""
+    submit.test_submit(15, mid=mid, eid=eid, is_checked=False)
+
+def checkpoint2():
+    """Completed latents for features subsubsection"""
+    submit.test_submit(16, mid=mid, eid=eid, is_checked=False)
+
+def checkpoint3():
+    """Completed GEMMASCOPE subsubsection"""
+    submit.test_submit(17, mid=mid, eid=eid, is_checked=False)
+
+def checkpoint4():
+    """Completed Section 1 Bonus"""
+    submit.test_submit(18, mid=mid, eid=eid, is_checked=False)
+
+def checkpoint5():
+    """Completed Section 2"""
+    submit.test_submit(19, mid=mid, eid=eid, is_checked=False)
+
+def checkpoint6():
+    """Completed Section 3"""
+    submit.test_submit(20, mid=mid, eid=eid, is_checked=False)
+
+def checkpoint7():
+    """Reached the end of the module"""
+    submit.test_submit(21, mid=mid, eid=eid, is_checked=False)
