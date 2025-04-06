848ad7057b1be9df94c02ac3c20a78d676d2dfdc
ivanenclonar <paulivanenclonar@gmail.com>
2025-04-06 17:43:57 +0800

    Updated the checkpoints and test_submit number

--- module_tests.py
+++ module_tests.py
@@ -12,12 +12,12 @@ def test1(logits_without_sae_recon, logits_no_saes):
     t.testing.assert_close(logits_no_saes, logits_without_sae_recon)
     print("All tests passed!")
 
-    submit.test_submit(12, mid=mid, eid=eid)
+    submit.test_submit(1, mid=mid, eid=eid)
 
 def test2(show_top_logits, gpt2, gpt2_sae):
     tests.test_show_top_logits(show_top_logits, gpt2, gpt2_sae)
     print("All tests passed!")
-    submit.test_submit(13, mid=mid, eid=eid)
+    submit.test_submit(2, mid=mid, eid=eid)
 
 def test3(prompts):
     assert prompts["system"].startswith("We're studying neurons in a neural network.")
@@ -25,33 +25,25 @@ def test3(prompts):
     assert prompts["assistant"] == "this neuron fires on"
 
     print("All tests passed!")
-    submit.test_submit(14, mid=mid, eid=eid)
+    submit.test_submit(3, mid=mid, eid=eid)
 
 #Checkpoints are not required for this module
 def checkpoint1():
-    """Completed Attention SAEs subsubsection"""
-    submit.test_submit(15, mid=mid, eid=eid, is_checked=False)
+    """1 - Completed Attention SAEs subsubsection"""
+    submit.test_submit(4, mid=mid, eid=eid, is_checked=False)
 
 def checkpoint2():
-    """Completed latents for features subsubsection"""
-    submit.test_submit(16, mid=mid, eid=eid, is_checked=False)
+    """2 - Completed GemmaScope and SAE Circuits subsubsection"""
+    submit.test_submit(5, mid=mid, eid=eid, is_checked=False)
 
 def checkpoint3():
-    """Completed GEMMASCOPE subsubsection"""
-    submit.test_submit(17, mid=mid, eid=eid, is_checked=False)
+    """3 - Completed Transcoders subsubsection"""
+    submit.test_submit(6, mid=mid, eid=eid, is_checked=False)
 
 def checkpoint4():
-    """Completed Section 1 Bonus"""
-    submit.test_submit(18, mid=mid, eid=eid, is_checked=False)
+    """2 - Completed section 3"""
+    submit.test_submit(7, mid=mid, eid=eid, is_checked=False)  
 
 def checkpoint5():
-    """Completed Section 2"""
-    submit.test_submit(19, mid=mid, eid=eid, is_checked=False)
-
-def checkpoint6():
-    """Completed Section 3"""
-    submit.test_submit(20, mid=mid, eid=eid, is_checked=False)
-
-def checkpoint7():
-    """Reached the end of the module"""
-    submit.test_submit(21, mid=mid, eid=eid, is_checked=False)
+    """Completed section 4"""
+    submit.test_submit(8, mid=mid, eid=eid, is_checked=False)
